ab5860771eef6dbc02a5fd8401a3b49987e03c8f
Tanner Percival <percivaltanner@gmail.com>
2019-12-17 09:23:51 -0800

    adds update position method

--- CodecademyConsoleGame/Game.cs
+++ CodecademyConsoleGame/Game.cs
@@ -4,6 +4,32 @@ namespace ConsoleGame
 {
   class Game : SuperGame
   {
-    
+    public new static void UpdatePosition(string key, out int x, out int y)
+        {
+            /*if(key == "DownArrow"){
+                y++;
+            }else if(key == "UpArrow"){
+                y--;
+            }else if(key == "LeftArrow"){
+                x++;
+            }else if(key == "RightArrow"){
+                x--;
+            }*/
+            switch (key){
+            case "DownArrow":
+                    y++;
+                    break;
+            case "UpArrow":
+                    y--;
+                    break;
+            case "LeftArrow":
+                    x++;
+                    break;
+            case "RightArrow":
+                    x--;
+                    break;
+            }
+	
+        }
   }
 }
